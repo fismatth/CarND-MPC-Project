1d3586a4d18c6f8d90406e4387a8b65900ec1870
Matthias Fischer <ma.fischer91@gmail.com>
2018-01-18 22:47:33 +0100

    Implemented MPC. Added writeup.

--- src/MPC.h
+++ src/MPC.h
@@ -6,15 +6,27 @@
 
 using namespace std;
 
+double polyeval(Eigen::VectorXd coeffs, double x);
+
 class MPC {
- public:
-  MPC();
+public:
+	MPC();
 
-  virtual ~MPC();
+	virtual ~MPC();
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+	// Solve the model given an initial state and polynomial coefficients.
+	// Return the first actuatotions.
+	vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+
+	std::vector<double> x_predicted_;
+	std::vector<double> y_predicted_;
+	std::vector<double> last_actuators_;
+
+	static size_t N;
+	static double dt;
+	static const double delay_s;
+	static const size_t NUM_ACTUATIONS;
+	static size_t delay_steps_;
 };
 
 #endif /* MPC_H */
